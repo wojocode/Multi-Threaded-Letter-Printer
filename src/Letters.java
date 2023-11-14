c71cf2a5b4fed22fe0a08d7c991a5051ea79c447
Wojocode <Wojciech665@gmail.com>
2023-11-14 13:26:07 +0100

    Initial commit

---
+++ src/Letters.java
@@ -0,0 +1,47 @@
+/**
+ * @ Wojciech Natkaniec
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Letters extends Thread {
+    private final String input;
+    private List<Thread> threads = new ArrayList<>();
+    private final int totalThreadsRuntimeInSeconds;
+    private final int threadIntervalInSeconds;
+
+    public Letters(String input, int totalThreadsRuntimeInSeconds, int threadRunTimeInterval) {
+        this.input = input;
+        this.totalThreadsRuntimeInSeconds = totalThreadsRuntimeInSeconds;
+        this.threadIntervalInSeconds = threadRunTimeInterval;
+        for (int i = 0; i < input.length(); i++) {
+            int cursor = i;
+            Thread thread = new Thread(() -> {
+                while (Thread.currentThread().isAlive()) {
+                    try {
+                        Thread.sleep(getThreadIntervalInSeconds());
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.print(input.charAt(cursor));
+                }
+            });
+            thread.setDaemon(true);
+            thread.setName("Thread " + input.charAt(cursor));
+            threads.add(thread);
+        }
+    }
+
+    public List<Thread> getThreads() {
+        return threads;
+    }
+
+    public int getTotalThreadsRuntimeInSeconds() {
+        return totalThreadsRuntimeInSeconds * 1000;
+    }
+
+    public int getThreadIntervalInSeconds() {
+        return threadIntervalInSeconds * 1000;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Letters letters = new Letters("ABCD", 5, 1);
+        for (Thread t : letters.getThreads()) System.out.println(t.getName());
+        for (Thread thread : letters.getThreads()) thread.start();
+
+        Thread.sleep(letters.getTotalThreadsRuntimeInSeconds());
+        System.out.println("\nProgram finished");
+    }
+}
+
